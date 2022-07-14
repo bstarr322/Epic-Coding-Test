7030770f1f11a73bc70c4caa9949f2569d74c5b8
bstarr322 <jointstarsoft@gmail.com>
2022-07-14 15:44:24 -0500

    Hotfix > Update footer styles

--- epicvue/wp-content/themes/epicvue/footer.php
+++ epicvue/wp-content/themes/epicvue/footer.php
@@ -7,26 +7,27 @@
  */
 $inverted_logo = get_field('inverted_logo', 'option');
 ?>
-<footer class="footer container text-center background-blue d-lg-flex justify-content-between py-20">
-	<a class="text-center" rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">
-		<img class="main-logo" src="<?php echo $inverted_logo['url'] ?>" alt="<?php echo $inverted_logo['alt'] ?: 'logo' ?>">
-	</a>
-	
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location'  => 'footer',
-			'container_class' => 'navbar-container toggle-offcanvas d-lg-flex algin-content-center mr-lg-100',
-			'container_id'    => 'navbarNavDropdown',
-			'menu_class'      => 'navbar-nav footer-menu ml-auto',
-			'fallback_cb'     => '',
-			'menu_id'         => 'footer',
-			'depth'           => 2,
-			'walker'          => new EpicPress_WP_Bootstrap_Navwalker(),
-			'hover dropdown'  => true,
-		)
-	);
-	?>
+<footer class="footer text-center background-blue d-lg-flex justify-content-between py-20">
+	<div class="container"></div>
+		<a class="text-center" rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">
+			<img class="main-logo" src="<?php echo $inverted_logo['url'] ?>" alt="<?php echo $inverted_logo['alt'] ?: 'logo' ?>">
+		</a>
+		
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'footer',
+				'container_class' => 'navbar-container toggle-offcanvas d-lg-flex algin-content-center mr-lg-100',
+				'container_id'    => 'navbarNavDropdown',
+				'menu_class'      => 'navbar-nav footer-menu ml-auto',
+				'fallback_cb'     => '',
+				'menu_id'         => 'footer',
+				'depth'           => 2,
+				'walker'          => new EpicPress_WP_Bootstrap_Navwalker(),
+				'hover dropdown'  => true,
+			)
+		);
+		?>
 </footer>
 
 <!-- DO NOT REMOVE  -->
