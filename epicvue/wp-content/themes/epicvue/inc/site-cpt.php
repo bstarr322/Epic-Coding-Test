26dc08b25e3eaa276c57ef0d1e54a84bd58df3cc
bstarr322 <jointstarsoft@gmail.com>
2022-07-14 11:07:33 -0500

    Add Post type > location

--- epicvue/wp-content/themes/epicvue/inc/site-cpt.php
+++ epicvue/wp-content/themes/epicvue/inc/site-cpt.php
@@ -1,6 +1,36 @@
 <?php 
-//Put your CPT Here 
-
-
+  //Put your CPT Here 
+  function register_location_post_type()
+  {
+    /**
+     * Post Type: Location.
+     */
+    $labels = array(
+        "name" => __("Locations", "epicvue"),
+        "singular_name" => __("Location", "epicvue"),
+    );
+    $args = array(
+        "label" => __("Locations", "epicvue"),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "has_archive" => false,
+        "show_in_menu" => true,
+        'menu_icon' => 'dashicons-admin-site',
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        'rewrite' => array('slug' => 'locations', 'with_front' => false),
+        "query_var" => true,
+        'supports' => array( 'title', 'editor', 'custom-fields', 'post-thumbnails' )
+    );
+    register_post_type("location", $args);
+  }
+  add_action('init', 'register_location_post_type');
 
  ?>
